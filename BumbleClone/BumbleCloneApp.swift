d548291b39c6a2c9966ce2561b9261b9a699035c
Abdullah <abdullaharslan1473@gmail.com>
2024-11-07 09:10:41 +0300

    Initial Commit

---
+++ BumbleClone/BumbleCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  BumbleCloneApp.swift
+//  BumbleClone
+//
+//  Created by abdullah on 07.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct BumbleCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
